b87ecd40c9260ec9b98cc52fa9254efb684ab8b4
operando <okanoshinobu8@gmail.com>
2014-10-31 22:03:25 +0900

    Add fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+class Fixnum
+	def fizzbuzz
+		return "FizzBuzz" if self % 15 == 0
+		return "Buzz" if self % 5 == 0
+		return "Fizz" if self % 3 == 0
+		self
+	end
+end
+
+puts (1..100).map(&:fizzbuzz)
